7cab5afbcf0561557db333982e20d944fe418ebb
Sibbir Ahmmed Sihan <69257025+sashan90s@users.noreply.github.com>
2024-02-17 19:58:10 +0000

    Update arrays_csharp.cs

--- arrays_csharp.cs
+++ arrays_csharp.cs
@@ -4,29 +4,33 @@ using System;
 
 namespace MyApplication
 {
-  class Program
-  {
-    static void Main()
-    { // Inserting at the end of an Array
-    	int[] intArray = new int[6];
-        // Make a variable to store the length because .length is based off capacity
-        // and does not keep track of actual indexing
-        int length = 0;
-        
-        // This adds data to for us
-        for (int i = 0; i < 3; i++)
-        {
-        	intArray[length] = i; 
+    class Program
+    {
+        static void Main()
+        { // Inserting at the end of an Array
+            int[] intArray = new int[6];
+            // Make a variable to store the length because .length is based off capacity
+            // and does not keep track of actual indexing
+            int length = 0;
+
+            // This adds data to for us
+            for (int i = 0; i < 3; i++)
+            {
+                intArray[length] = i;
+                length++;
+                //Console.WriteLine(intArray[length]);
+            }
+
+            intArray[length] = 8;
             length++;
+
+            // below line of code is not working for some reason and I do not know why
+            foreach (int value in intArray)
+            {
+                Console.WriteLine(value);
+            }
             //Console.WriteLine(intArray[length]);
         }
-        
-        intArray[length] = 8;
-        length++;
-        
-        // below line of code is not working for some reason and I do not know why
-        Console.WriteLine(intArray[length]);
     }
-  }
 }
 
