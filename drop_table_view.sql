c9ca92cba9a499f0621c7c2398bd4d59aa60c816
Sibbir Ahmmed Sihan <69257025+sashan90s@users.noreply.github.com>
2024-02-19 16:47:20 +0000

    Create drop_table_view.sql

---
+++ drop_table_view.sql
@@ -0,0 +1,19 @@
+
+Use db
+GO
+
+IF (SELECT OBJECT_ID('[db].[tmp].[vw_details]')) IS NOT NULL 
+DROP VIEW [tmp].[vw_details]
+
+
+GO
+
+
+CREATE VIEW [tmp].[vw_details]] AS
+
+
+-- code goes here after improment
+
+Select * from db.tmp.tbl_working_days
+
+GO
