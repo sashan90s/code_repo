17dcb535de57a1e078ca788a525b311e89b0e329
Sibbir Ahmmed Sihan <69257025+sashan90s@users.noreply.github.com>
2024-09-27 15:55:25 +0100

    Update conditional_SELECT.sql
    Updated code added

--- conditional_SELECT.sql
+++ conditional_SELECT.sql
@@ -23,3 +23,29 @@ Begin
 	SELECT count (*)
 	FROM [DATAMART_SOCIAL_CARE].[ade].[vw_all_visits_FY_start_date_24_25]
 End
+
+
+
+-- This following bit is also important for the new one.
+
+DECLARE @dec INT;
+
+
+-- Count the number of rows in the table and assign to @dec
+SELECT @dec = COUNT(*)
+FROM [DATAMART_SOCIAL_CARE].[ade].[vw_all_visits_FY_start_date_24_25]
+Where ([Provider name] = @IndividualID or [event_start_date] = @BirthDate) ;
+
+-- Check if the count is less than 1
+IF @dec >= 1
+BEGIN
+    -- selecting all values where parameters are matched
+    SELECT *
+    FROM [DATAMART_SOCIAL_CARE].[ade].[vw_all_visits_FY_start_date_24_25]
+	Where ([Provider name] = @IndividualID or [event_start_date] = @BirthDate);
+END
+
+ELSE
+Begin
+	SELECT ''
+End
