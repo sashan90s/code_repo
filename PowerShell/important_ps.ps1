9193d8020393cfbd94dff1a5c99fcf3a19468c21
Sibbir Sihan <sibbir.sihan@ofsted.gov.uk>
2024-10-01 14:42:48 +0100

    some changes on ps

--- PowerShell/important_ps.ps1
+++ PowerShell/important_ps.ps1
@@ -11,3 +11,23 @@ Write-Host "Today's date $($datevar)"
 Write-Host "90 days before today $($datevar.AddDays(-90))"
 
 
+
+$pw = Get-ChildItem $pshome/powershell.exe
+
+Write-host $pw
+
+# $pw|gm
+
+# $pw|gm -MemberType Property
+
+$pw.CreationTime
+
+$pw.CreationTime
+
+$justdate = $pw.CreationTime
+
+$justdate.ToShortDateString() # it is a method so you gotta use bracket
+
+
+
+
