3399d165fa91fb1b3326755342f6c3d6c7ec18a3
Sibbir Sihan <sibbir.sihan@ofsted.gov.uk>
2024-10-01 10:55:10 +0100

    all

---
+++ PowerShell/test_app.ps1
@@ -0,0 +1,30 @@
+﻿function Show-Menu() {
+
+Write-Host "================ $Title ================"
+
+Write-Host "1: Press '1' for this option."
+Write-Host "2: Press '2' for this option."
+Write-Host "3: Press '3' for this option."
+Write-Host "Q: Press 'Q' to quit."
+}
+
+do {
+    Show-Menu
+    $input = Read-Host "Please make a selection"
+    switch ($input) {
+        '1' {
+            cls
+            'You chose option #1'
+        } '2' {
+            cls
+            'You chose option #2'
+        } '3' {
+            cls
+            'You chose option #3'
+        } 'q' {
+            return
+        }
+    }}
+    until ($input -eq 'q')
+    
+
